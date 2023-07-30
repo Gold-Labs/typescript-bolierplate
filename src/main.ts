335480860839fa71bf819fecc0e01017addc8608
player31-kks <keumks789@naver.com>
2023-07-30 21:50:22 +0900

    [Fix] upgrade package

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log("hello")
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.end('Hello World');
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
